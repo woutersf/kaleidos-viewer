f6cd58cd45572ab8d679c8b886d829f14e5811e2
Michaël Dierick <michael.dierick@redpencil.io>
2019-05-27 18:49:25 +0200

    Added support for rendering snippets

--- addon/components/kaleidos-viewer.js
+++ addon/components/kaleidos-viewer.js
@@ -1,8 +1,31 @@
 import Component from '@ember/component';
 import layout from '../templates/components/kaleidos-viewer';
+import DS from 'ember-data';
+import fetch from 'fetch';
+import { get } from '@ember/object';
+import { computed } from '@ember/object';
 
 export default Component.extend({
   layout,
   
-  documentVersion: undefined
+  documentVersion: undefined,
+  
+  htmlSnippet: computed('documentVersion.convertedFile', function () {
+    if( this.documentVersion && this.documentVersion.convertedFile )
+    {
+      console.log(`Fetching converted file`);
+      const url = new URL(`/files/${get(this,'documentVersion.convertedFile.id')}/download`, window.location.href);
+      return DS.PromiseObject.create({
+        promise: fetch(url).then((response) => {
+          return response.text()
+        })
+      });
+    } else {
+      return '';
+    }
+})
+
 });
+
+
+
